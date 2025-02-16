0ad8671b96cad3e92a3e81736d6744df5c4edbb3
Haresh Patel <Maharship343@gmail.com>
2025-02-16 16:06:01 +0530

    add img thing

--- Mov.js
+++ Mov.js
@@ -1,5 +1,4 @@
 // fetch(
-//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}&api_key=359744bb5c3efbf6336f333857bf59ca"
 // )
 //   .then((data) => {
 //     console.log("Data received:", data);
